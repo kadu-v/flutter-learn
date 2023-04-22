e8d443e50c4db0340a7577e590e73079b9b625db
kadu-v <kazuonsunday@yahoo.co.jp>
2023-04-22 16:57:29 +0900

    Finish：chapter 12

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -71,17 +71,46 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Row(
+          children: const [Icon(Icons.create), Text("初めてのタイトル")],
+        ),
       ),
-      body: Column(
-          children: [const Text("Hello Wrold"), const Text("Hello World")]),
+      body: Column(children: [
+        const Text("Hello Wrold"),
+        const Text("Hello World"),
+        TextButton(
+            onPressed: () => {
+                  print(
+                    "ボタンが押されたよ",
+                  )
+                },
+            child: const Text("テキストボタン")),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: const [
+          Icon(
+            Icons.favorite,
+            color: Colors.pink,
+            size: 24.0,
+          ),
+          Icon(
+            Icons.audiotrack,
+            color: Colors.green,
+            size: 30.0,
+          ),
+          Icon(
+            Icons.beach_access,
+            color: Colors.blue,
+            size: 36.0,
+          ),
+        ]),
+      ]),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => {print("押したね？")},
+        child: const Icon(Icons.timer),
+      ),
+      drawer: const Drawer(
+        child: Center(child: Text("Drawer")),
+      ),
+      endDrawer: const Drawer(child: Center(child: Text("EndDrawer"))),
     );
   }
 }
